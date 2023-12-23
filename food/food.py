62f18f693f1bce520e06a1412b8a0b5a98654f6f
Brunowcst <brunoporfirio022@gmail.com>
2023-12-22 21:03:02 -0300

    Food

---
+++ food/food.py
@@ -0,0 +1,16 @@
+import random
+import pygame
+from configs.configs import width, square_size, height, screen, blue
+
+
+def generate_food(pixels):
+    while True:
+        food_x = round(random.randrange(0, width - square_size) / 10.0) * 10.0
+        food_y = round(random.randrange(0, height - square_size) / 10.0) * 10.0
+
+        # Verifica se a comida não está na posição da cobra
+        if [food_x, food_y] not in pixels:
+            return food_x, food_y
+
+def draw_food(size, food_x, food_y):
+    pygame.draw.rect(screen, blue, [food_x, food_y, size, size])
