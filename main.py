e98d880176382a5c96003b0e3a708a9d38ffa405
Brunowcst <brunoporfirio022@gmail.com>
2023-12-22 21:03:07 -0300

    Update

--- main.py
+++ main.py
@@ -1,114 +1,14 @@
 import sys
 import pygame
-import random
+
+from configs.configs import *
+from snake.snake import draw_snake
+from messages.messages import draw_pause_message, draw_score, game_over_dialog
+from food.food import draw_food, generate_food
+from controls.controls import select_speed
 
 pygame.init()
 pygame.display.set_caption("Snake game")
-width, height = 400, 300
-screen = pygame.display.set_mode((width, height))
-clock = pygame.time.Clock()
-
-black = (0, 0, 0)
-white = (255, 255, 255)
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
-
-square_size = 10
-game_speed = 15
-
-def generate_food(pixels):
-    while True:
-        food_x = round(random.randrange(0, width - square_size) / 10.0) * 10.0
-        food_y = round(random.randrange(0, height - square_size) / 10.0) * 10.0
-
-        # Verifica se a comida não está na posição da cobra
-        if [food_x, food_y] not in pixels:
-            return food_x, food_y
-
-def draw_food(size, food_x, food_y):
-    pygame.draw.rect(screen, blue, [food_x, food_y, size, size])
-
-def draw_snake(size, pixels):
-    for pixel in pixels:
-        pygame.draw.rect(screen, white, [pixel[0], pixel[1], size, size])
-
-    if len(pixels) > 0:
-        head_x, head_y = pixels[-1]
-        eye_size = size // 3
-        eye_offset = size // 10
-
-        eye1_x = head_x + eye_offset
-        eye1_y = head_y + eye_offset
-        eye2_x = head_x + size - eye_offset - eye_size
-        eye2_y = head_y + eye_offset
-
-        pygame.draw.rect(screen, red, [eye1_x, eye1_y, eye_size, eye_size])
-        pygame.draw.rect(screen, red, [eye2_x, eye2_y, eye_size, eye_size])
-
-def draw_score(score):
-    font = pygame.font.SysFont("Helvetica", 15)
-    text = font.render(f"Score: {score}", True, green)
-    screen.blit(text, [1, 1])
-
-def draw_pause_message():
-    font = pygame.font.SysFont("Helvetica", 15)
-    text = font.render("Paused - Press any key to continue", True, white)
-    text_rect = text.get_rect(center=(width // 2, height // 2))
-    screen.blit(text, text_rect)
-
-def select_speed(key, current_speed):
-    speed_x, speed_y = current_speed
-
-    if key == pygame.K_DOWN and speed_y != -square_size:
-        speed_x = 0
-        speed_y = square_size
-    elif key == pygame.K_UP and speed_y != square_size:
-        speed_x = 0
-        speed_y = -square_size
-    elif key == pygame.K_RIGHT and speed_x != -square_size:
-        speed_x = square_size
-        speed_y = 0
-    elif key == pygame.K_LEFT and speed_x != square_size:
-        speed_x = -square_size
-        speed_y = 0
-    elif key == pygame.K_s and speed_y != -square_size:
-        speed_x = 0
-        speed_y = square_size
-    elif key == pygame.K_w and speed_y != square_size:
-        speed_x = 0
-        speed_y = -square_size
-    elif key == pygame.K_d and speed_x != -square_size:
-        speed_x = square_size
-        speed_y = 0
-    elif key == pygame.K_a and speed_x != square_size:
-        speed_x = -square_size
-        speed_y = 0
-    elif key == pygame.K_p or key == pygame.K_SPACE:
-        speed_x = 0
-        speed_y = 0
-    else:
-        return current_speed
-    return speed_x, speed_y
-
-def game_over_dialog():
-    font = pygame.font.SysFont("Helvetica", 15)
-    text = font.render("Game Over! Play again? (Y/N)", True, white)
-    text_rect = text.get_rect(center=(width // 2, height // 2))
-    screen.blit(text, text_rect)
-    pygame.display.update()
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_y:
-                    return True
-                elif event.key == pygame.K_n:
-                    pygame.quit()
-                    sys.exit()
 
 def exec_game():
     end_game = False
